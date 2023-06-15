a1dd5ace0012c17818b250226cd4abf59487f832
Aditya Urkude <67952339+Nomad-adi@users.noreply.github.com>
2023-06-15 14:08:33 +0530

    update staticfiles

--- automate_news_poster/settings.py
+++ automate_news_poster/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-p69gd$jq4&()g=1kfis1!&f)9&xty2ulzl!a7)6&n3=4ey=s7_'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ['.vercel.app']
+ALLOWED_HOSTS = ['.vercel.app',"*"]
 
 
 # Application definition
